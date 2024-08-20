749e52080f80a3922fc47aa5b6c05ea48a5d9c83
dawei <879987375@qq.com>
2024-08-20 14:48:37 +0800

    增加对excel的处理

--- AutoOrder/main/main.go
+++ AutoOrder/main/main.go
@@ -10,6 +10,21 @@ import (
 var indexUrl string = "https://vip.stock.finance.sina.com.cn/mkt/"
 
 func main() {
+
+    // 连续竞价，监控、委托
+    // AutoOrderMain()
+
+    // 集合竞价数据收集、分析
+    // autoOrder.GetCallAuctionInfo()
+
+    // 生成监控config
+    // SetConfigFile()
+
+    // 生成excel文件
+    autoOrder.SetStockExcel()
+}
+
+func AutoOrderMain() {
     err := autoOrder.LoadConfig()
     if err != nil {
         fmt.Println(err)
@@ -19,19 +34,17 @@ func main() {
 
     go autoOrder.MoniterConfigFile()
 
+Main_Loop:
     for {
-        clct := autoOrder.InitSinaQuoteSpider(1)
-        // 发起调用
-        clct.Visit(indexUrl)
-
-        time.Sleep(700 * time.Millisecond)
+        select {
+        case <-time.After(700 * time.Millisecond):
+            clct := autoOrder.InitSinaQuoteSpider(1)
+            // 发起调用
+            clct.Visit(indexUrl)
+        case <-time.After(6 * time.Hour):
+            break Main_Loop
+        }
     }
-
-    // 集合竞价数据收集、分析
-    // autoOrder.GetCallAuctionInfo()
-
-    // 生成监控config
-    // SetConfigFile()
 }
 
 // 生成监控config
